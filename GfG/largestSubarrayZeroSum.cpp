098465ee9f132ba9b2cea93e296a3b6a850ed30a
Aashutosh Rathi <aashutoshrathi@gmail.com>
2019-08-01 00:22:25 +0530

    Largest subarray with 0 sum

---
+++ GfG/largestSubarrayZeroSum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxLen(int arr[], int n){
+    int sum = 0;
+    int len = INT_MIN;
+    unordered_map<int ,int> mp;
+    
+    for(int i=0; i< n; i++) {
+        sum += (arr[i] == 0) ? -1 : 1;
+        if(sum==0) {
+            len = max(len, i+1);
+        }
+        if(mp.find(sum) != mp.end()) {
+            len = max(len, i-mp[sum]);
+        } else {
+            mp[sum] = i;
+        }
+    }
+    if(len == INT_MIN)
+        return 0;
+    return len;
+    
+}
