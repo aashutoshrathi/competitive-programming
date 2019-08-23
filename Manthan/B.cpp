29be4b1a20527953a419066035bd0e1234ba810d
Aashutosh Rathi <aashutoshrathi@gmail.com>
2019-08-24 00:23:27 +0530

    Add Sig Tree

---
+++ Manthan/B.cpp
@@ -0,0 +1,42 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~ Aashutosh Rathi~~~~~~
+// ~~~~~~~~aashutoshrathi~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// https://codeforces.com/problemset/problem/489/C
+ 
+// Use memcpy while copying matrix :D
+// o_O Never do calculation in if block -_- 
+ 
+#include <bits/stdc++.h> 	// Apne aap me sab kuch hai isme 
+#include <stdio.h>
+
+using namespace std;	// Kyuki yahi mai-baap hai
+ 
+#define mod(x) 			x%1000000007;
+#define MIN(n1, n2)     ((n1) > (n2) ? (n2) : (n1))
+#define MAX(n1, n2)     ((n1) > (n2) ? (n1) : (n2))
+#define MID(s, e)       (s+(e-s)/2)
+#define fast fin; fout; fio;
+#define fin cin.tie(NULL)
+#define fout cout.tie(NULL)
+#define fio ios::sync_with_stdio(false)
+typedef pair <long, long> lpairs;
+typedef long long ll;
+#define fcout(a, n) \
+for(ll i = 0; i < n; i++) \
+    printf("%lld ", a[i]); \
+    printf("\n");
+
+
+int main() {
+    fast
+    int t;
+    cin >> t;
+    ll a[t];
+    for(int i=0; i<t; i++) {
+        cin >> a[i];
+    }
+
+    return 0;
+}
+
