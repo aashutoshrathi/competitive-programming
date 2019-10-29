1c4dd7e80dfc0c2fe34c32117cc5014c86f21748
Aashutosh Rathi <aashutoshrathi@gmail.com>
2019-10-29 14:45:46 +0530

    Add TwoSum

---
+++ LeetCode/TwoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int, int> ss; 
+        for(int i=0; i<nums.size(); i++) {
+            int x = target-nums[i];
+            if(ss.count(x)) {
+                res.push_back(ss[x]);
+                res.push_back(i);
+                break;
+            }
+            ss[nums[i]] = i;
+        }
+        return res;
+    }
+};
