640c4b64f90566c0d63a4e55df4ef53574ea1faa
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-06-06 22:44:12 +0530

    Move to atan2 for precise slope

--- FooBar/gun_to_guard_fight.py
+++ FooBar/gun_to_guard_fight.py
@@ -2,38 +2,38 @@
 Bringing a Gun to a Guard Fight (Given: 15 Days)
 ===============================
 
-Uh-oh - you've been cornered by one of Commander Lambdas elite guards! Fortunately, 
-you grabbed a beam weapon from an abandoned guard post while you were running through 
-the station, so you have a chance to fight your way out. But the beam weapon is potentially 
-dangerous to you as well as to the elite guard: its beams reflect off walls, meaning you'll have 
+Uh-oh - you've been cornered by one of Commander Lambdas elite guards! Fortunately,
+you grabbed a beam weapon from an abandoned guard post while you were running through
+the station, so you have a chance to fight your way out. But the beam weapon is potentially
+dangerous to you as well as to the elite guard: its beams reflect off walls, meaning you'll have
 to be very careful where you shoot to avoid bouncing a shot toward yourself!
 
-Luckily, the beams can only travel a certain maximum distance before becoming too 
-weak to cause damage. You also know that if a beam hits a corner, it will bounce back 
-in exactly the same direction. And of course, if the beam hits either you or the guard, 
-it will stop immediately (albeit painfully). 
+Luckily, the beams can only travel a certain maximum distance before becoming too
+weak to cause damage. You also know that if a beam hits a corner, it will bounce back
+in exactly the same direction. And of course, if the beam hits either you or the guard,
+it will stop immediately (albeit painfully).
 
-Write a function solution(dimensions, your_position, guard_position, distance) that 
-gives an array of 2 integers of the width and height of the room, an array of 2 integers 
-of your x and y coordinates in the room, an array of 2 integers of the guard's x and y 
-coordinates in the room, and returns an integer of the number of distinct directions that you 
+Write a function solution(dimensions, your_position, guard_position, distance) that
+gives an array of 2 integers of the width and height of the room, an array of 2 integers
+of your x and y coordinates in the room, an array of 2 integers of the guard's x and y
+coordinates in the room, and returns an integer of the number of distinct directions that you
 can fire to hit the elite guard, given the maximum distance that the beam can travel.
 
-The room has integer dimensions [1 < x_dim <= 1250, 1 < y_dim <= 1250]. You and the 
-elite guard are both positioned on the integer lattice at different distinct positions (x, y) 
-inside the room such that [0 < x < x_dim, 0 < y < y_dim]. Finally, the maximum distance 
-that the beam can travel before becoming harmless will be given as an 
+The room has integer dimensions [1 < x_dim <= 1250, 1 < y_dim <= 1250]. You and the
+elite guard are both positioned on the integer lattice at different distinct positions (x, y)
+inside the room such that [0 < x < x_dim, 0 < y < y_dim]. Finally, the maximum distance
+that the beam can travel before becoming harmless will be given as an
 integer 1 < distance <= 10000.
 
 For example, if you and the elite guard were positioned in a room with dimensions [3, 2],
-your_position [1, 1], guard_position [2, 1], and a maximum shot distance of 4, 
-you could shoot in seven different directions to hit the elite guard 
-(given as vector bearings from your location): 
-[1, 0], [1, 2], [1, -2], [3, 2], [3, -2], [-3, 2], and [-3, -2]. 
-As specific examples, the shot at bearing [1, 0] is the straight line horizontal 
-shot of distance 1, the shot at bearing [-3, -2] bounces off the left wall and then the 
-bottom wall before hitting the elite guard with a total shot distance of sqrt(13), 
-and the shot at bearing [1, 2] bounces off just the top wall before hitting the 
+your_position [1, 1], guard_position [2, 1], and a maximum shot distance of 4,
+you could shoot in seven different directions to hit the elite guard
+(given as vector bearings from your location):
+[1, 0], [1, 2], [1, -2], [3, 2], [3, -2], [-3, 2], and [-3, -2].
+As specific examples, the shot at bearing [1, 0] is the straight line horizontal
+shot of distance 1, the shot at bearing [-3, -2] bounces off the left wall and then the
+bottom wall before hitting the elite guard with a total shot distance of sqrt(13),
+and the shot at bearing [1, 2] bounces off just the top wall before hitting the
 elite guard with a total shot distance of sqrt(5).
 
 
@@ -50,20 +50,29 @@ solution.solution([300,275], [150,150], [185,100], 500)
 Output:
     9
 '''
-
+from math import atan2
 TWO = 2
 
 
-def unique_hash(pt):
+def sign(a):
     '''
-    return unique value for pt
+    signum function
     '''
-    return '{}#{}'.format(pt[0], pt[1])
+    return (a > 0) - (a < 0)
+
+
+def gcd(x, y):
+    '''
+    Fast GCD
+    '''
+    while(y):
+        x, y = y, x % y
+    return x
 
 
 def reflect(pt, mirror):
     '''
-    return reflection of point 
+    return reflection of point
     (normally with vertical axes and 0)
     '''
     return mirror*TWO - pt
@@ -72,12 +81,18 @@ def reflect(pt, mirror):
 def solution(dim, you, guard, distance):
     d_two = distance**TWO
 
+    def unique_hash(pt):
+        '''
+        return slope of bearing vector
+        '''
+        return atan2(you[0]-pt[0], you[1]-pt[1])
+
     def not_me(dst, src=you):
         '''
         returns whether this is same point
         as where we began.
         '''
-        return (dst[0] != src[0]) and (dst[1] != src[1])
+        return (dst[0] != src[0]) and (dst[1] != src[0])
 
     def euclid_distance(dst, src=you):
         '''
@@ -130,24 +145,28 @@ def solution(dim, you, guard, distance):
             for rfl in get_all_reflections(curr_pt):
                 if not_me(rfl):
                     pseudo_valid_pts_q.append(rfl)
-
+    # print(history)
     return len(history)
 
 
-
 if __name__ == "__main__":
-    # dimensions = [2, 5]
-    # your_position = [1, 2]
-    # guard_position = [1, 4]
-    # distance = 11
+    dimensions = [2, 5]
+    your_position = [1, 2]
+    guard_position = [1, 4]
+    distance = 11
 
     # dimensions = [300, 275]
     # your_position = [150, 150]
     # guard_position = [185, 100]
     # distance = 500
 
-    dimensions = [3, 2]
-    your_position = [1, 1]
-    guard_position = [2, 1]
-    distance = 4
+    # dimensions = [3, 2]
+    # your_position = [1, 1]
+    # guard_position = [2, 1]
+    # distance = 4
+
+    # dimensions = [23,10]
+    # your_position = [6, 4]
+    # guard_position = [3,2]
+    # distance = 23
     print(solution(dimensions, your_position, guard_position, distance))
