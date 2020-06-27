b0bbe64e53690462e654cc17e850f579bb1c1cbc
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-06-27 14:50:21 +0530

    Add W4P6: numSquares (Math approach)

--- LeetCode/JuneChallenge/Week4/numSquares.cpp
+++ LeetCode/JuneChallenge/Week4/numSquares.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Solution {
 public:
+    // 1. DP Approach
     int numSquares(int n) {
         vector<int> ways(n+1);
         // fill ways[i] = i;
@@ -16,6 +17,30 @@ public:
         }
 
         return ways[n];
+    }
+
+    // 2. Pure Math (0ms, Source: LC Dicussions)
+    // As per https://en.wikipedia.org/wiki/Lagrange%27s_four-square_theorem
+    // Every number can be represented as sum of 4 perfect sq.
+
+    bool checkSq(int n) {
+        int floor_root = floor(sqrt(n));
+        return (floor_root * floor_root) == n;
+    }
+
+    int numSquares(int n) {
+        int froot = floor(sqrt(n));
+        if(checkSq(n))
+            return 1;
+        
+        for(int i=1; i <= froot; i++)
+            if(checkSq(n - i*i))
+                return 2;
+        
+        while(!(n%4))
+            n >>= 2;
+    
+        return 3 + (n%8 == 7);
     }
 };
 
